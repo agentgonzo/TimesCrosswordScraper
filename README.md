d2047f9f11f931f1da6c5ac95313d5a5e6747fa0
Steve Arch <sarch@cloudbees.com>
2019-11-17 17:27:07 +0000

    More awesomeness

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # TimesCrosswordScraper
-A Scraper for the Times Crosswords
+A Scraper for the Crosswords from The Times Crossword Club. It will download crosswords of the specified type in the input data range. It also has the option (TBD) to print out the crosswords automatically if you are running on a linux system with a printer enabled (it uses `lp`)
+
+A subscription is required to access The Times crosswords (obviously, because nothing comes for free... even if you voted for Brexit)
+
+I'd like to make this easily deployable/integratable with a web server for easy printing, but for the moment, it's CLI only.
+
+## Usage
+Run `python crossoword.py`.
+
+On first login, you will be propmted for some login information (cookies - see below).
+You must specify a date range and crossword type.
+
+### Command line arguments
+* `-s <date>` Start date - get crosswords from this date. Date input in DD/MM/YYYY format
+* `-e <date>` End date - get crosswords until this date. Date input in DD/MM/YYYY format
+* `-t <type>` Crossword type - see below for the list of types
+* `-p` (Optional) Print out the crosswords after downloading
+
+### Crossword Types
+* `6`: Sunday Times Concise
+* `5`: Times Concise
+* `7`: Times Concise Jumbo
+* `1`: Quick Cryptic
+* `3`: Sunday Times Cryptic
+* `2`: Times Cryptic
+* `4`: Times Cryptic Jumbo
+* `9`: General Knowledge Jumbo
+* `8`: Mephisto
+* `12`: Monthly Club Special
+* `10`: O Tempora! (Latin)
+* `11`: The Listener
+* `SPECIALIST`: Specialist
+* `CRYPTIC`: Cryptic
+
+## Login/Authentication
+The login mechanism is awkward to simulate, so for the time being, you will have to log in via your web browser, and then copy two cookies into the application.
+These cookies are:
+* `acs_tnl`
+* `sacs_tnl`
+
+The login mechanism involves not only a standard `POST` with the username and password, but also a `GET` with a signed JWT. I haven't traced how to get the login working with just the username and password yet, so but if you can figure it out, then please contact me or submit a PR. Fortunately, the two cookies are long-lived (in the order of months) so once you have set them you don't need to reset them that often.
+
--- crossword.py
+++ crossword.py
@@ -3,11 +3,10 @@ import pickle
 import urllib
 from getpass import getpass
 
-import requests
-import yaml
-from bs4 import BeautifulSoup
-from enum import Enum
-
+# import requests
+# import yaml
+# from bs4 import BeautifulSoup
+# from enum import Enum
 
 class CrosswordFetcher:
     SESSION_FILE = '.sessionfile'
@@ -54,29 +53,22 @@ class CrosswordFetcher:
         keyvalues = [param for param in params.split('&')]
         return {param.split('=')[0]: param.split('=')[1] for param in keyvalues}
 
-    def get_page(self, url):
-        results = self.session.get(url, allow_redirects=False)
-        if results.status_code == 200:
-            return results
-        else:  # logged out
-            self.login()
-            get = self.session.get(url)
-            return get
-
     def __del__(self):
         self.save_session()
 
-    def get_crosswords(self, start, end):
-        search_page = self.construct_url(start, end)
+    def get_crosswords(self, crossword_type, start, end, to_print=False):
+        search_page = self.construct_url(crossword_type, start, end)
         pdfs = self.get_crosswords_from_url(search_page)
         print('Downloading {count} crosswords'.format(count=len(pdfs)))
         for pdf in pdfs:
-            self.download_and_save(pdf)
+            filename = self.download_and_save(pdf)
+            if to_print:
+                self.print_crossword(filename)
 
     def get_crosswords_from_url(self, url):
         r = self.session.get(url)
         if not r.ok:
-            raise Exception("Couldn't get official results from", url, r)
+            raise Exception("Couldn't get crosswords from", url, r)
 
         b = BeautifulSoup(r.text, features='html.parser')
         # Get all the printable links
@@ -86,29 +78,71 @@ class CrosswordFetcher:
         if len(links) == 0:
             return links
         try:
-            show_more_link=next(f for f in b.find_all('a', {'class': 'Item-cta Link--primary'}) if f.text=='Show more')['href']
+            show_more_link = \
+                next(f for f in b.find_all('a', {'class': 'Item-cta Link--primary'}) if f.text == 'Show more')['href']
             return links + self.get_crosswords_from_url(show_more_link)
         except StopIteration:  # No link to more. Nothing to do
             pass
             return links
 
     def download_and_save(self, url):
+        '''
+        Downloads and saves a PDF from the given URL to the current working directory
+        :param url: the URL (hopefully a PDF) to download
+        :return: the filename of the saved file
+        '''
         resp = self.session.get(url)
         print('Saving ' + url)
         fd = open(url.split('/')[-1] + '.pdf', 'wb')
         fd.write(resp.content)
         fd.close()
+        return fd.name  # Checkme
 
-    def construct_url(self, start, end):
-        return 'https://www.thetimes.co.uk/puzzleclub/crosswordclub/puzzles-list?search=&filter[puzzle_type]={type}&filter[publish_at][from]={start}&filter[publish_at][to]={end}'\
-            .format(type=CrosswordType.QUICK_CRYPTIC, start=start, end=end)
+    def construct_url(self, crossword_type, start, end):
+        return 'https://www.thetimes.co.uk/puzzleclub/crosswordclub/puzzles-list?search=&filter[puzzle_type]={type}&filter[publish_at][from]={start}&filter[publish_at][to]={end}' \
+            .format(type=crossword_type, start=start, end=end)
+
+    def print_crossword(self, filename):
+        '''
+        Prints a file (to the printer, not stdout). Very rudimentary, assumes the default printer
+        :param filename: the file to print
+        '''
+        print('Printing ' + filename)
+        os.execv('lp', filename)
 
 
-class CrosswordType(Enum):
-    QUICK_CRYPTIC = 1
-
+crossword_types = {
+    "6": "Sunday Times Concise",
+    "5": "Times Concise",
+    "7": "Times Concise Jumbo",
+    "1": "Quick Cryptic",
+    "3": "Sunday Times Cryptic",
+    "2": "Times Cryptic",
+    "4": "Times Cryptic Jumbo",
+    "9": "General Knowledge Jumbo",
+    "8": "Mephisto",
+    "12": "Monthly Club Special",
+    "10": "O Tempora! (Latin)",
+    "11": "The Listener",
+    "SPECIALIST": "Specialist",
+    "CRYPTIC": "Cryptic",
+}
 
 if __name__ == '__main__':
-    CrosswordFetcher().get_crosswords('01/09/2019', '30/09/2019')
+    # Todo: Parse CLI args
 
+    crossword_type = "hutne"
+    while crossword_type not in crossword_types:  # get type from flags
+        print ("Crossword types: ")
+        for t in crossword_types:
+            print(t + ": " + crossword_types[t])
+        crossword_type = raw_input("Which Crossword Type?")
 
+    if True:  # get start date from flags
+        start = raw_input("Start Date (dd/mm/yyyy")
+    if True:  # get end date from flags
+        end = raw_input("End Date (dd/mm/yyyy")
+    # if not to_print:
+    #     to_print = raw_input("Print downloaded crosswords?")
+    to_print = False  # get this from user input
+    CrosswordFetcher().get_crosswords(crossword_type, start, end, to_print=to_print)
